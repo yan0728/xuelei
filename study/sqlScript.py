c444a7a27f81b25ec8481c67140a3d643866f72f
yanxuelei <xueleiyan@jieyuechina.com>
2020-06-16 15:06:39 +0800

    读取excel文件并保存到数据库内

--- study/sqlScript.py
+++ study/sqlScript.py
@@ -5,6 +5,17 @@
 @file: sqlScript.py
 @time:2020/5/13 0013
 """
-import connectMysql
+import xlrd
 
 
+def excel():
+    wb = xlrd.open_workbook("D:\python37\学习\捷越直接放款债权明细.xlsx")
+    sheet = wb.sheet_by_name("Sheet1")
+    for a in range(sheet.nrows):
+        cells = sheet.row_values(a)
+        if '*' in cells[3]:
+            del cells[3]
+        elif '*' in cells[3]:
+            print(cells[3])
+
+excel()
